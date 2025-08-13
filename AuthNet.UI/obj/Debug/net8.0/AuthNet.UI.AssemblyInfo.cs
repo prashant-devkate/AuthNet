9a53c742d126671cdbdec56b2e87d72f8121e24a
prashant devkate <prashantdevkate1999@gmail.com>
2025-08-13 15:43:10 +0530

    Add some dummy data

--- AuthNet.UI/obj/Debug/net8.0/AuthNet.UI.AssemblyInfo.cs
+++ AuthNet.UI/obj/Debug/net8.0/AuthNet.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthNet.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a40cad4be2cf7a51d49ec157543c2c29e81375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642367630e31131cd358de1858103e9c6b25fde1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthNet.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthNet.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
